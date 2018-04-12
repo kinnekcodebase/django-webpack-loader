5457dbb7be46e42c6c20170c0aefd0ecb41e982a
Jie Zhou <jie.z.zhou@gmail.com>
2018-04-12 14:37:08 -0400

    added get_url func

--- webpack_loader/utils.py
+++ webpack_loader/utils.py
@@ -30,41 +30,7 @@ def get_files(bundle_name, extension=None, config='DEFAULT'):
     '''Returns list of chunks from named bundle'''
     return list(_get_bundle(bundle_name, extension, config))
 
-
-# def get_as_tags(bundle_name, extension=None, config='DEFAULT', attrs=''):
-#     '''
-#     Get a list of formatted <script> & <link> tags for the assets in the
-#     named bundle.
-#     :param bundle_name: The name of the bundle
-#     :param extension: (optional) filter by extension, eg. 'js' or 'css'
-#     :param config: (optional) the name of the configuration
-#     :return: a list of formatted tags as strings
-#     '''
-
-#     bundle = _get_bundle(bundle_name, extension, config)
-#     tags = []
-#     for chunk in bundle:
-#         if chunk['name'].endswith(('.js', '.js.gz')):
-#             tags.append((
-#                 '<script type="text/javascript" src="{0}" {1}></script>'
-#             ).format(chunk['url'], attrs))
-#         elif chunk['name'].endswith(('.css', '.css.gz')):
-#             tags.append((
-#                 '<link type="text/css" href="{0}" rel="stylesheet" {1}/>'
-#             ).format(chunk['url'], attrs))
-#     return tags
-
-
-# def get_static(asset_name, config='DEFAULT'):
-#     '''
-#     Equivalent to Django's 'static' look up but for webpack assets.
-#     :param asset_name: the name of the asset
-#     :param config: (optional) the name of the configuration
-#     :return: path to webpack asset as a string
-#     '''
-#     return "{0}{1}".format(
-#         get_loader(config).get_assets().get(
-#             'publicPath', getattr(settings, 'STATIC_URL')
-#         ),
-#         asset_name
-#     )
+def get_url(bundle_name, extension=None, config='DEFAULT'):
+    '''Returns first url of files from named bundle'''
+    files = get_files(bundle_name, extension, config)
+    return files[0].get('url') if files and files[0] and files[0].get('url') else None
