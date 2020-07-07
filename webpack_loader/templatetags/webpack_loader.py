86ed996001d626553b02a559c8cab60070455a85
Johann Cutiongco <johann@kinnek.com>
2020-07-07 19:01:04 -0400

    removed unnecessary code

--- webpack_loader/templatetags/webpack_loader.py
+++ webpack_loader/templatetags/webpack_loader.py
@@ -6,34 +6,6 @@ from .. import utils
 register = template.Library()
 
 
-def filter_by_extension(bundle, extension):
-    '''Return only files with the given extension'''
-    for chunk in bundle:
-        if chunk['name'].endswith('.{0}'.format(extension)):
-            yield chunk
-
-
-def render_as_tags(bundle):
-    tags = []
-    for chunk in bundle:
-        if chunk['name'].endswith('.js'):
-            tags.append((
-                '<script type="text/javascript" src="{0}"></script>'
-            ).format(chunk['url']))
-        elif chunk['name'].endswith('.css'):
-            tags.append((
-                '<link type="text/css" href="{0}" rel="stylesheet"/>'
-            ).format(chunk['url']))
-    return mark_safe('\n'.join(tags))
-
-
-def _get_bundle(bundle_name, extension, config):
-    bundle = utils.get_loader(config, bundle_name).get_bundle(bundle_name)
-    if extension:
-        bundle = filter_by_extension(bundle, extension)
-    return bundle
-
-
 @register.simple_tag
 def render_bundle(bundle_name, extension=None, config='DEFAULT', attrs=''):
     tags = utils.get_as_tags(bundle_name, extension=extension, config=config, attrs=attrs)
@@ -63,11 +35,3 @@ def get_files(bundle_name, extension=None, config='DEFAULT'):
     :return: a list of matching chunks
     """
     return utils.get_files(bundle_name, extension=extension, config=config)
-
-
-@register.assignment_tag
-def get_url(bundle_name, extension=None, config='DEFAULT'):
-    """
-    Returns first file url
-    """
-    return utils.get_first_url(bundle_name, extension=extension, config=config)
